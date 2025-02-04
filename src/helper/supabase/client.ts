94df3746e0aa4341a467e1d1eff2020d3d09940b
hoshiirui <shibaharumii@gmail.com>
2025-02-04 22:07:47 +0800

    client

--- src/helper/supabase/client.ts
+++ src/helper/supabase/client.ts
@@ -3,7 +3,7 @@ import { createBrowserClient } from "@supabase/ssr";
 export function createClient() {
   // Create a supabase client on the browser with project's credentials
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    "https://nqzspgzcbmwdwnzdhbrl.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xenNwZ3pjYm13ZHduemRoYnJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc1NDUyNTgsImV4cCI6MjA1MzEyMTI1OH0.2SOsuH8TlJ5AFgilPAv7_F2UPqfQ-9WLOLGvQ6X6Rho",
   );
 }
