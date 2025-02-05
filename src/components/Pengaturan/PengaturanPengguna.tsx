6a903d2852e74e50ef4b8357e09f3636994f0977
hoshiirui <shibaharumii@gmail.com>
2025-02-05 10:19:06 +0800

    fixing pengaturan bug

--- src/components/Pengaturan/PengaturanPengguna.tsx
+++ src/components/Pengaturan/PengaturanPengguna.tsx
@@ -374,7 +374,7 @@ const PengaturanPengguna = () => {
         )}
 
         <div className="mt-8 grid grid-cols-5 gap-8">
-          <div className="col-span-5 xl:col-span-3">
+          <div className="col-span-full">
             <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
               <div className="border-b border-stroke px-7 py-4 dark:border-strokedark">
                 <h3 className="font-medium text-black dark:text-white">
